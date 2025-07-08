9d1fdc438b653cdc4b99dc3da9f64be1f76d9d52
Vagner Nerves <vagnernervessantos@gmail.com>
2025-07-08 08:30:37 -0300

    feat: add getRooms on register

--- server/src/server.ts
+++ server/src/server.ts
@@ -8,6 +8,8 @@ import {
 
 import { env } from './env.ts'
 
+import { getRoomsRoute } from './http/routes/get-rooms.ts'
+
 const app = fastify().withTypeProvider<ZodTypeProvider>()
 
 app.register(fastifyCors, {
@@ -20,6 +22,8 @@ app.setValidatorCompiler(validatorCompiler)
 app.get('/health', () => {
   return 'OK'
 })
+
+app.register(getRoomsRoute)
 
 app.listen({
   port: env.PORT,
