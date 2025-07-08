659ad68b0e2da1935436d54147e07604c4a88396
Vagner Nerves <vagnernervessantos@gmail.com>
2025-07-08 08:53:57 -0300

    feat: Ajusted lint this archives with biome

--- web/src/main.tsx
+++ web/src/main.tsx
@@ -1,11 +1,12 @@
-import "./index.css";
+import './index.css'
 
-import { StrictMode } from "react";
-import { createRoot } from "react-dom/client";
-import { App } from "./app.tsx";
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import { App } from './app.tsx'
 
-createRoot(document.getElementById("root")!).render(
+// biome-ignore lint/style/noNonNullAssertion: mandatory by React
+createRoot(document.getElementById('root')!).render(
   <StrictMode>
     <App />
   </StrictMode>
-);
+)
