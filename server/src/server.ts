a3ece57888cb0441320a820485247d563c2db3f9
Vagner Nerves <vagnernervessantos@gmail.com>
2025-07-08 17:44:51 -0300

    feat: Create Room Route Post and add server

--- server/src/server.ts
+++ server/src/server.ts
@@ -7,7 +7,7 @@ import {
 } from 'fastify-type-provider-zod'
 
 import { env } from './env.ts'
-
+import { createRoomRoute } from './http/routes/create-room.ts'
 import { getRoomsRoute } from './http/routes/get-rooms.ts'
 
 const app = fastify().withTypeProvider<ZodTypeProvider>()
@@ -24,6 +24,7 @@ app.get('/health', () => {
 })
 
 app.register(getRoomsRoute)
+app.register(createRoomRoute)
 
 app.listen({
   port: env.PORT,
