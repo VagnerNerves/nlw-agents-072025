0b894f87e5145b7824fce438f6c531ab39b78254
Vagner Nerves <vagnernervessantos@gmail.com>
2025-07-08 17:31:04 -0300

    feat: Add 20 rooms

--- server/src/db/seed.ts
+++ server/src/db/seed.ts
@@ -7,7 +7,7 @@ await reset(db, schema)
 await seed(db, schema).refine((f) => {
   return {
     rooms: {
-      count: 5,
+      count: 20,
       columns: {
         name: f.companyName(),
         description: f.loremIpsum(),
